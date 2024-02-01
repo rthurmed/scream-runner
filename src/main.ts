eedf7e87099257ac455e0067a0bf219b85e6ccef
Arthur <rthurmedeiros@gmail.com>
2024-02-01 12:37:26 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import kaboom from "kaboom";
+
+const GAME_HEIGHT = 600;
+const GAME_WIDTH = 800;
+const GAME_GRAVITY = 1600;
+const GAME_BACKGROUND = [164, 209, 250];
+const FLOOR_SIZE = 100;
+
+const k = kaboom({
+  height: GAME_HEIGHT,
+  width: GAME_WIDTH,
+  background: GAME_BACKGROUND
+});
+
+k.debug.inspect = true;
+
+k.loadSprite('bean', '/sprites/bean.png');
+
+k.setGravity(2400);
+
+k.add([
+  k.pos(0, GAME_HEIGHT - FLOOR_SIZE),
+  k.rect(GAME_WIDTH, FLOOR_SIZE),
+  k.color(k.Color.BLACK),
+  k.area(),
+  k.body({
+    isStatic: true
+  })
+])
+
+const player = k.add([
+  k.sprite('bean'),
+  k.pos(100, 100),
+  k.area(),
+  k.body()
+]);
