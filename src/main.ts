543cc7730ead25316c17f26e2e5a5a3e1d255074
Arthur <rthurmedeiros@gmail.com>
2024-03-02 10:48:27 -0300

    Implemented starting and gameover scenes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import kaboom, { GameObj, KaboomCtx, OpacityComp } from "kaboom";
-import { Microphone } from "./microphone";
+import { IMicrophone, Microphone } from "./microphone";
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
@@ -20,6 +20,7 @@ const PLAYER_WALK_POSITION = PLAYER_STEP * 2;
 const PLAYER_MAX_POSITION = PLAYER_STEP * 3;
 const PLAYER_SPEED = 600;
 const PLAYER_LIFE = 100;
+const PLAYER_DEATH_TIMEOUT = 2;
 const VOLUME_RAISE = GAME_TICK * 16;
 const VOLUME_DECAY = GAME_TICK * 2;
 const ENEMY_FALLING_Y = 10;
@@ -60,6 +61,7 @@ export const addPlayer = (k: KaboomCtx) => {
       width: SPRITE_SCALED_SIZE,
       height: SPRITE_SCALED_SIZE
     }),
+    k.opacity(1),
     k.anchor("bot"),
     k.pos(100, 100),
     k.area({
@@ -115,7 +117,6 @@ export const addPlayer = (k: KaboomCtx) => {
   });
 
   player.onCollide("projectile", (projectile, collision) => {
-    // TODO: death
     player.hurt(ENEMY_DAMAGE);
     projectile.destroy();
   });
@@ -133,6 +134,16 @@ export const addPlayer = (k: KaboomCtx) => {
   player.onHurt(() => {
     k.play("hurt", randPitch(k));
     k.shake(30);
+  });
+
+  player.onDeath(() => {
+    // TODO: death animation
+    player.opacity = 0;
+    player.paused = true;
+    player.isStatic = true;
+    k.wait(PLAYER_DEATH_TIMEOUT, () => {
+      k.go("gameover");
+    });
   });
 
   return player;
@@ -261,6 +272,271 @@ export const addCoin = (k: KaboomCtx) => {
   return coin;
 }
 
+const makeGameScene = (k: KaboomCtx, microphone: IMicrophone, debug: boolean = false) => {
+  return () => {
+    // boundaries
+    const floor = k.add([
+      "structure",
+      k.pos(-BOUNDARY_SIZE, k.height() - FLOOR_SIZE),
+      k.rect(k.width() + (BOUNDARY_SIZE * 2), FLOOR_SIZE),
+      k.color(k.Color.fromHex('#372538')),
+      k.area(),
+      k.body({
+        isStatic: true
+      })
+    ]);
+    const boundaries = [
+      k.add([ // left boundary
+        "boundary",
+        k.pos(-BOUNDARY_SIZE, -BOUNDARY_SIZE),
+        k.rect(BOUNDARY_SIZE, k.height() + (BOUNDARY_SIZE * 2)),
+        k.area(),
+        k.body({
+          isStatic: true
+        })
+      ]),
+      k.add([ // right boundary
+        "boundary",
+        k.pos(k.width(), -BOUNDARY_SIZE),
+        k.rect(BOUNDARY_SIZE, k.height() + (BOUNDARY_SIZE * 2)),
+        k.area(),
+        k.body({
+          isStatic: true
+        })
+      ]),
+      k.add([ // bottom boundary
+        "boundary",
+        k.pos(-BOUNDARY_SIZE, k.height()),
+        k.rect(k.width() + (BOUNDARY_SIZE * 2), BOUNDARY_SIZE),
+        k.area(),
+        k.body({
+          isStatic: true
+        })
+      ]),
+      k.add([ // top boundary
+        "boundary",
+        k.pos(-BOUNDARY_SIZE, -BOUNDARY_SIZE),
+        k.rect(k.width() + (BOUNDARY_SIZE * 2), BOUNDARY_SIZE),
+        k.area(),
+        k.body({
+          isStatic: true
+        })
+      ])
+    ];
+
+    // player
+    const player = addPlayer(k);
+
+    // ui
+    const uiLifeHeight = k.height() - QUARTER_SPRITE_SIZE * 2;
+    const uiCollectibleHeight = QUARTER_SPRITE_SIZE + UI_ICON_PADDING;
+
+    const iconLife = k.add([
+      k.sprite("heart", {
+        width: SPRITE_SCALED_SIZE,
+        height: SPRITE_SCALED_SIZE
+      }),
+      k.pos(-QUARTER_SPRITE_SIZE + UI_ICON_PADDING, uiLifeHeight),
+      k.anchor("left")
+    ]);
+
+    // NOTE: hidden
+    const iconCollectible = k.add([
+      k.opacity(0),
+      k.sprite("cake", {
+        width: SPRITE_SCALED_SIZE,
+        height: SPRITE_SCALED_SIZE
+      }),
+      k.pos(-QUARTER_SPRITE_SIZE + UI_ICON_PADDING, uiCollectibleHeight),
+      k.anchor("left")
+    ]);
+    const textCollectible = k.add([
+      k.text("Score: 0", {
+        size: 24,
+      }),
+      // k.pos(quarterSpriteSize * 2.5, uiCollectibleHeight),
+      k.pos(UI_ICON_PADDING * 3, uiCollectibleHeight),
+      k.anchor("left"),
+    ])
+
+    const lifeBackground = k.add([
+      k.pos(QUARTER_SPRITE_SIZE * 2.5, k.height() - UI_ICON_PADDING),
+      k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT),
+      k.anchor("botleft"),
+      k.color(k.Color.fromHex("#7A213A"))
+    ]);
+
+    const lifeDisplay = k.add([
+      k.pos(QUARTER_SPRITE_SIZE * 2.5, k.height() - UI_ICON_PADDING),
+      k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT),
+      k.anchor("botleft"),
+      k.color(k.Color.fromHex("#e14141"))
+    ]);
+
+    const darkRed = k.Color.fromHex("#7a213a");
+    const red = k.Color.fromHex("#e14141");
+    const orange = k.Color.fromHex("#ffbf36");
+    const yellow = k.Color.fromHex("#fff275");
+
+    const iconVolume = k.add([
+      k.sprite("microphone", {
+        width: SPRITE_SCALED_SIZE,
+        height: SPRITE_SCALED_SIZE
+      }),
+      k.pos(k.width() - UI_ICON_PADDING * 6, uiLifeHeight + 4),
+      k.anchor("right")
+    ])
+    
+    const rulerTemplate = [
+      k.pos(k.width() - UI_ICON_PADDING, k.height() - UI_ICON_PADDING),
+      k.anchor("botright"),
+    ]
+
+    k.add([...rulerTemplate, k.color(darkRed), k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT)]);
+    k.add([...rulerTemplate, k.color(red),     k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT * MIC_LEVEL_3)]);
+    k.add([...rulerTemplate, k.color(orange),  k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT * MIC_LEVEL_2)]);
+    k.add([...rulerTemplate, k.color(yellow),  k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT * MIC_LEVEL_1)]);
+
+    const volumeDisplay = k.add([
+      k.pos(k.width() - UI_ICON_PADDING * 4, k.height() - UI_ICON_PADDING),
+      k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT),
+      k.anchor("botright"),
+      k.color(k.Color.WHITE),
+      k.outline(4, k.Color.fromHex("#372538"))
+    ]);
+
+    // enemies
+    if (debug) {
+      k.onKeyPress('1', () => (addFallingEnemy(k, PLAYER_MIN_POSITION)));
+      k.onKeyPress('2', () => (addFallingEnemy(k, PLAYER_WALK_POSITION)));
+      k.onKeyPress('3', () => (addFallingEnemy(k, PLAYER_MAX_POSITION)));
+      k.onKeyPress('4', () => (addWalkingEnemy(k)));
+      k.onKeyPress('5', () => (addFlyingEnemy(k)));
+    }
+    
+    k.wait(GAME_INITIAL_TIME, () => {
+      k.loop(ENEMY_FALLING_SPAWN_RATE, () => {
+        const spawnXPosition = k.choose([
+          PLAYER_MIN_POSITION,
+          PLAYER_MIN_POSITION,
+          PLAYER_MIN_POSITION,
+          PLAYER_WALK_POSITION,
+          PLAYER_WALK_POSITION,
+          PLAYER_MAX_POSITION,
+        ])
+        addFallingEnemy(k, spawnXPosition);
+      });
+    });
+
+    k.wait(ENEMY_HORIZONTAL_INITIAL_TIME, () => {
+      k.loop(ENEMY_HORIZONTAL_SPAWN_RATE, () => {
+        const spawnPattern: SpawnPattern = k.choose([
+          'both',
+          'flying',
+          'flying',
+          'flying',
+          'walking',
+          'walking',
+        ]);
+        if (spawnPattern == 'flying' || spawnPattern == 'both') {
+          addFlyingEnemy(k);
+        }
+        if (spawnPattern == 'walking' || spawnPattern == 'both') {
+          addWalkingEnemy(k);
+        }
+      });
+    });
+
+    // collectibles
+    if (debug) {
+      k.onKeyPress('q', () => (addCoin(k)));
+    }
+
+    k.wait(COLLECTIBLE_INITIAL_TIME, () => {
+      k.loop(COLLECTIBLE_SPAWN_RATE, () => {
+        if (k.chance(.75)) {
+          addCoin(k);
+        }
+      });
+    });
+
+    // game state
+    let volume = 0;
+    let coins = 0;
+
+    // game loop
+    k.loop(GAME_TICK, () => {
+      // manage volume
+      const rawVolume = microphone.getVolume();
+      const speed = rawVolume > volume ? VOLUME_RAISE : VOLUME_DECAY;
+      volume = k.lerp(volume, rawVolume, speed);
+
+      // update volume display
+      volumeDisplay.height = volume * 80;
+
+      // update player display
+      lifeDisplay.height = UI_BAR_HEIGHT * (player.hp() / player.maxHP());
+      textCollectible.text = `Score: ${player.collected.toString()}`;
+
+      // player movement
+      player.updateState(volume);
+      player.moveTo(player.destX, player.pos.y, PLAYER_SPEED);
+
+      // player jump
+      const tryingToJump = volume > MIC_LEVEL_2;
+      if (tryingToJump && player.isGrounded()) {
+        player.jump(PLAYER_JUMP_STRENGTH);
+      }
+    });
+
+    // autostart
+    k.play("music", {
+      loop: true,
+      volume: .3,
+    });
+  }
+}
+
+const makeStartScene = (k: KaboomCtx, microphone: IMicrophone, debug: boolean = false) => {
+  return () => {
+    k.add([
+      k.sprite("enzo", {
+        width: SPRITE_SCALED_SIZE,
+        height: SPRITE_SCALED_SIZE
+      }),
+      k.pos(k.width() / 2, k.height() / 2 + 100 - (UI_ICON_PADDING * 4)),
+      k.anchor("bot")
+    ]);
+    k.add([
+      k.text('Scream to start!', {
+        align: "center",
+      }),
+      k.pos(k.width() / 2, k.height() / 2 + 100),
+      k.anchor("top")
+    ]);
+    k.loop(GAME_TICK, () => {
+      const rawVolume = microphone.getVolume();
+      if (rawVolume > MIC_LEVEL_3) {
+        k.go("game");
+      }
+    });
+  }
+}
+
+const makeGameoverScene = (k: KaboomCtx, microphone: IMicrophone, debug: boolean = false) => {
+  return () => {
+    k.add([
+      k.text('Game over!', {
+        align: "center",
+        letterSpacing: 8,
+      }),
+      k.pos(k.width() / 2, k.height() / 2),
+      k.anchor("center")
+    ]);
+    // TODO: retry button
+  }
+}
+
 const main = async ({ debug = true }) => {
   const k = kaboom({
     width: GAME_WIDTH,
@@ -270,7 +546,7 @@ const main = async ({ debug = true }) => {
   });
   
   // assets
-  // sprites  
+  // sprites
   k.loadSpriteAtlas("sprites/spritesheet.png", {
     "cake": {
       height: SPRITESHEET_SIZE,
@@ -339,228 +615,13 @@ const main = async ({ debug = true }) => {
   // modules
   const microphone = await Microphone();
 
-  // boundaries
-  const floor = k.add([
-    "structure",
-    k.pos(-BOUNDARY_SIZE, k.height() - FLOOR_SIZE),
-    k.rect(k.width() + (BOUNDARY_SIZE * 2), FLOOR_SIZE),
-    k.color(k.Color.fromHex('#372538')),
-    k.area(),
-    k.body({
-      isStatic: true
-    })
-  ]);
-  const boundaries = [
-    k.add([ // left boundary
-      "boundary",
-      k.pos(-BOUNDARY_SIZE, -BOUNDARY_SIZE),
-      k.rect(BOUNDARY_SIZE, k.height() + (BOUNDARY_SIZE * 2)),
-      k.area(),
-      k.body({
-        isStatic: true
-      })
-    ]),
-    k.add([ // right boundary
-      "boundary",
-      k.pos(k.width(), -BOUNDARY_SIZE),
-      k.rect(BOUNDARY_SIZE, k.height() + (BOUNDARY_SIZE * 2)),
-      k.area(),
-      k.body({
-        isStatic: true
-      })
-    ]),
-    k.add([ // bottom boundary
-      "boundary",
-      k.pos(-BOUNDARY_SIZE, k.height()),
-      k.rect(k.width() + (BOUNDARY_SIZE * 2), BOUNDARY_SIZE),
-      k.area(),
-      k.body({
-        isStatic: true
-      })
-    ]),
-    k.add([ // top boundary
-      "boundary",
-      k.pos(-BOUNDARY_SIZE, -BOUNDARY_SIZE),
-      k.rect(k.width() + (BOUNDARY_SIZE * 2), BOUNDARY_SIZE),
-      k.area(),
-      k.body({
-        isStatic: true
-      })
-    ])
-  ];
+  k.scene("game", makeGameScene(k, microphone, debug));
+  k.scene("start", makeStartScene(k, microphone, debug));
+  k.scene("gameover", makeGameoverScene(k, microphone, debug));
 
-  // player
-  const player = addPlayer(k);
-
-  // ui
-  const uiLifeHeight = k.height() - QUARTER_SPRITE_SIZE * 2;
-  const uiCollectibleHeight = QUARTER_SPRITE_SIZE + UI_ICON_PADDING;
-
-  const iconLife = k.add([
-    k.sprite("heart", {
-      width: SPRITE_SCALED_SIZE,
-      height: SPRITE_SCALED_SIZE
-    }),
-    k.pos(-QUARTER_SPRITE_SIZE + UI_ICON_PADDING, uiLifeHeight),
-    k.anchor("left")
-  ]);
-
-  // NOTE: hidden
-  const iconCollectible = k.add([
-    k.opacity(0),
-    k.sprite("cake", {
-      width: SPRITE_SCALED_SIZE,
-      height: SPRITE_SCALED_SIZE
-    }),
-    k.pos(-QUARTER_SPRITE_SIZE + UI_ICON_PADDING, uiCollectibleHeight),
-    k.anchor("left")
-  ]);
-  const textCollectible = k.add([
-    k.text("Score: 0", {
-      size: 24,
-    }),
-    // k.pos(quarterSpriteSize * 2.5, uiCollectibleHeight),
-    k.pos(UI_ICON_PADDING * 3, uiCollectibleHeight),
-    k.anchor("left"),
-  ])
-
-  const lifeBackground = k.add([
-    k.pos(QUARTER_SPRITE_SIZE * 2.5, k.height() - UI_ICON_PADDING),
-    k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT),
-    k.anchor("botleft"),
-    k.color(k.Color.fromHex("#7A213A"))
-  ]);
-
-  const lifeDisplay = k.add([
-    k.pos(QUARTER_SPRITE_SIZE * 2.5, k.height() - UI_ICON_PADDING),
-    k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT),
-    k.anchor("botleft"),
-    k.color(k.Color.fromHex("#e14141"))
-  ]);
-
-  const darkRed = k.Color.fromHex("#7a213a");
-  const red = k.Color.fromHex("#e14141");
-  const orange = k.Color.fromHex("#ffbf36");
-  const yellow = k.Color.fromHex("#fff275");
-
-  const iconVolume = k.add([
-    k.sprite("microphone", {
-      width: SPRITE_SCALED_SIZE,
-      height: SPRITE_SCALED_SIZE
-    }),
-    k.pos(k.width() - UI_ICON_PADDING * 6, uiLifeHeight + 4),
-    k.anchor("right")
-  ])
-  
-  const rulerTemplate = [
-    k.pos(k.width() - UI_ICON_PADDING, k.height() - UI_ICON_PADDING),
-    k.anchor("botright"),
-  ]
-
-  k.add([...rulerTemplate, k.color(darkRed), k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT)]);
-  k.add([...rulerTemplate, k.color(red),     k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT * MIC_LEVEL_3)]);
-  k.add([...rulerTemplate, k.color(orange),  k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT * MIC_LEVEL_2)]);
-  k.add([...rulerTemplate, k.color(yellow),  k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT * MIC_LEVEL_1)]);
-
-  const volumeDisplay = k.add([
-    k.pos(k.width() - UI_ICON_PADDING * 4, k.height() - UI_ICON_PADDING),
-    k.rect(UI_BAR_WIDTH, UI_BAR_HEIGHT),
-    k.anchor("botright"),
-    k.color(k.Color.WHITE),
-    k.outline(4, k.Color.fromHex("#372538"))
-  ]);
-
-  // enemies
-  if (debug) {
-    k.onKeyPress('1', () => (addFallingEnemy(k, PLAYER_MIN_POSITION)));
-    k.onKeyPress('2', () => (addFallingEnemy(k, PLAYER_WALK_POSITION)));
-    k.onKeyPress('3', () => (addFallingEnemy(k, PLAYER_MAX_POSITION)));
-    k.onKeyPress('4', () => (addWalkingEnemy(k)));
-    k.onKeyPress('5', () => (addFlyingEnemy(k)));
-  }
-  
-  k.wait(GAME_INITIAL_TIME, () => {
-    k.loop(ENEMY_FALLING_SPAWN_RATE, () => {
-      const spawnXPosition = k.choose([
-        PLAYER_MIN_POSITION,
-        PLAYER_MIN_POSITION,
-        PLAYER_MIN_POSITION,
-        PLAYER_WALK_POSITION,
-        PLAYER_WALK_POSITION,
-        PLAYER_MAX_POSITION,
-      ])
-      addFallingEnemy(k, spawnXPosition);
-    });
-  });
-
-  k.wait(ENEMY_HORIZONTAL_INITIAL_TIME, () => {
-    k.loop(ENEMY_HORIZONTAL_SPAWN_RATE, () => {
-      const spawnPattern: SpawnPattern = k.choose([
-        'both',
-        'flying',
-        'flying',
-        'flying',
-        'walking',
-        'walking',
-      ]);
-      if (spawnPattern == 'flying' || spawnPattern == 'both') {
-        addFlyingEnemy(k);
-      }
-      if (spawnPattern == 'walking' || spawnPattern == 'both') {
-        addWalkingEnemy(k);
-      }
-    });
-  });
-
-  // collectibles
-  if (debug) {
-    k.onKeyPress('q', () => (addCoin(k)));
-  }
-
-  k.wait(COLLECTIBLE_INITIAL_TIME, () => {
-    k.loop(COLLECTIBLE_SPAWN_RATE, () => {
-      if (k.chance(.75)) {
-        addCoin(k);
-      }
-    });
-  });
-
-  // game state
-  let volume = 0;
-  let coins = 0;
-
-  // game loop
-  k.loop(GAME_TICK, () => {
-    // manage volume
-    const rawVolume = microphone.getVolume();
-    const speed = rawVolume > volume ? VOLUME_RAISE : VOLUME_DECAY;
-    volume = k.lerp(volume, rawVolume, speed);
-
-    // update volume display
-    volumeDisplay.height = volume * 80;
-
-    // update player display
-    lifeDisplay.height = UI_BAR_HEIGHT * (player.hp() / player.maxHP());
-    textCollectible.text = `Score: ${player.collected.toString()}`;
-
-    // player movement
-    player.updateState(volume);
-    player.moveTo(player.destX, player.pos.y, PLAYER_SPEED);
-
-    // player jump
-    const tryingToJump = volume > MIC_LEVEL_2;
-    if (tryingToJump && player.isGrounded()) {
-      player.jump(PLAYER_JUMP_STRENGTH);
-    }
-  });
-
-  // autostart
-  k.play("music", {
-    loop: true,
-    volume: .3,
-  });
+  k.go("start");
 }
 
 main({
-  debug: false
+  debug: true
 });
